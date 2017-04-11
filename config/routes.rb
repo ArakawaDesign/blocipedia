97b4fcd7469dfdf2cb7d19a00782613195c1687f
Ryan Arakawa <arakawa.ryan@gmail.com>
2017-04-11 21:13:07 +0000

    User Story 4 Wiki CRUD: Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :wikis
+
   devise_for :users
-  get 'welcome/index'
-  
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
   
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
